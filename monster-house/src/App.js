13c738518dad632b69481f463282fc2aa74f062e
Bogdan <bogdan.george.calancea@gmail.com>
2020-02-13 20:29:18 +0200

    Initial commit

---
+++ monster-house/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
