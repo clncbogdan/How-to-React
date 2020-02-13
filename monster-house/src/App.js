32c044dcd3c2502f0c9072a1cd9b5968071ca816
Bogdan <bogdan.george.calancea@gmail.com>
2020-02-13 23:23:18 +0200

    Display a list of monsters

--- monster-house/src/App.js
+++ monster-house/src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
-import ReactDOM from "react";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			monsters: [
+				{
+					id: '1',
+					name: 'Frankenstein',
+				},
+				{
+					id: '2',
+					name: 'Dracula',
+				},
+				{
+					id: '3',
+					name: 'Zombie',
+				},
+			],
+		};
+	}
+	render() {
+		return (
+			<div className="App">
+				{this.state.monsters.map(monster => (
+					<h2 key={monster.id}>{monster.name}</h2>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
